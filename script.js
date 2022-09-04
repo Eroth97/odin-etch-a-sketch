e36ad9ae8536fcadd30d4d053dc2c5671f6d8d13
David Tellez <davidctellezg@gmail.com>
2022-09-03 22:56:26 -0500

    Create the makeSquares function

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 let etchBox = document.getElementById('etch-box');
 
-for(let j = 0; j < 16; j++){
-    let row = document.createElement('div');
-    for (let i = 0; i < 16; i++){
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('px-5');
-        row.appendChild(newDiv);
+//Use this code to get a number from the user. 
+// let number = document.getElementById('number');
+
+//Create a button with an event listener to invoke the makeSquares function.
+makeSquares(50);
+
+
+//Put this code on a function to activate the hovering.
+let iterator = document.querySelectorAll('.new-div');
+
+iterator.forEach( (element) =>{
+    element.addEventListener('mouseover', (event) =>{
+        element.style.backgroundColor = 'black';
+    })
+})
+
+
+
+//This function would fill the Etch a Sketch box with squares.
+function makeSquares(squares){
+    let squaresSize = 500/squares;
+
+    console.log(squaresSize);
+    console.log(squares);
+
+    for(let j = 0; j < squares; j++){
+        let row = document.createElement('div');
+        for (let i = 0; i < squares; i++){
+            let newDiv = document.createElement('div');
+            newDiv.classList.add('new-div');
+            newDiv.style.height = `${squaresSize}px`;
+            newDiv.style.width = `${squaresSize}px`;
+            row.appendChild(newDiv);
+        }
+        etchBox.appendChild(row);
     }
-    etchBox.appendChild(row);
 }
+
